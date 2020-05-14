1b2369f58db6e718c40c69eb4a7a6e53037b4260
Rudy <arrouasserudy@hotmail.com>
2020-05-15 00:48:20 +0300

    add axios + api + cors in server

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin")
+const cors = require('cors')({origin: true});
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
@@ -15,22 +16,25 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 
 exports.getPublicEvent = functions.https.onRequest((req, res) => {
-    if(req.method !== 'GET') {
-        return res.status(401).json({
-            message: 'Not allowed'
-        })
-    }
-    const eventId = req.query.eventId
-    if (!eventId) {
-        return res.status(400).json({
-            message: 'Event id is needed'
-        })
-    }
-    endpoints.getEvent(db, eventId).then(event => {
-        return res.status(200).send(event)
-    }).catch(() => {
-        return res.status(400).json({
-            message: 'no event'
+    cors(req, res, () => {
+        if(req.method !== 'GET') {
+            return res.status(401).json({
+                message: 'Not allowed'
+            })
+        }
+        const eventId = req.query.eventId
+        if (!eventId) {
+            return res.status(400).json({
+                message: 'Event id is needed'
+            })
+        }
+        endpoints.getEvent(db, eventId).then(event => {
+            const {guests, ...rest} = event
+            return res.status(200).send(rest)
+        }).catch(() => {
+            return res.status(400).json({
+                message: 'no event'
+            })
         })
     })
 });

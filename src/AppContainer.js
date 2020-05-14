2cdc64a6fed8c1bedf6d947993002fce51860df1
Rudy <arrouasserudy@hotmail.com>
2020-05-15 00:10:35 +0300

    add routing

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import App from "./components/App/App";
 import style from './index.css';
 
+const Home = () => <div>Home</div>
+const Login = () => <div>Login</div>
+const PageNotFound = () => <div>PageNotFound 404</div>
 
 function AppContainer() {
-  return (
-    <div className={style.background}>
-      <App/>
-    </div>
-  );
+	return (
+		<Router>
+			<div className={style.background}>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/login" component={Login} />
+					<Route path="/event/:eventId" component={App} />
+					<Route component={PageNotFound} />
+				</Switch>
+			</div>
+		</Router>
+
+	);
 }
 
 export default AppContainer;

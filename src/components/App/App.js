a8374f93056928b080b43747cf073e4b14701a5b
unknown <yohavbenoliel@gmail.com>
2020-05-15 08:55:11 +0300

    templates logic created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,24 @@
 import React from 'react';
-import style from './App.module.css';
-import CountdownYB from '../Countdown/CountdownYB';
-import InvitText from '../InvitText/InvitText';
 
 import data from '../../data/data';
+import TemplateA from '../Templates/TemplateA';
+import TemplateB from '../Templates/TemplateB';
+import TemplateC from '../Templates/TemplateC';
 
+
+const _Template = {
+    a: TemplateA,
+    b: TemplateB,
+    c: TemplateC,
+}
 
 const App = () =>  {
-    const {name1, name2, title, bsd, date} = data
+    const { template } = data;
+
+    const Template = _Template[{ template }];
 
     return (
-        <div className={style.background}>
-            <div className={style.container}>
-                
-                <div className={style.bsd}>{bsd}</div>
-                <div className={style.name}>{title}</div>
-                <div className={style.names}>
-                    <div className={style.name}>
-                        {name1}
-                    </div>
-                    <div className={style.name}>
-                        {'&'}
-                    </div>
-                    <div className={style.name}>
-                        {name2}
-                    </div>
-                </div>
-                <div>
-                    <CountdownYB futurDate={date}> </CountdownYB>
-                </div>
-                <div className={style.details}>
-                    <InvitText></InvitText>
-                </div>
-            </div>
-        </div>
+     <Template data={data}/>
     );
 }
 

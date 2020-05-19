77442de3b7a1deb3f016771c5a09d0ee644d778a
Rudy <arrouasserudy@hotmail.com>
2020-05-19 19:35:54 +0300

    add setGuestData endpoint

--- server-deploy.sh
+++ server-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd server
-firebase deploy
+firebase deploy --only functions
--- server/functions/index.js
+++ server/functions/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')({origin: true});
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
-const endpoints = require("./db")
+const dbApi = require("./db")
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -28,12 +28,42 @@ exports.getPublicEvent = functions.https.onRequest((req, res) => {
                 message: 'Event id is needed'
             })
         }
-        endpoints.getEvent(db, eventId).then(event => {
+        dbApi.getEvent(db, eventId).then(event => {
             const {guests, ...rest} = event
             return res.status(200).send(rest)
         }).catch(() => {
             return res.status(400).json({
                 message: 'no event'
+            })
+        })
+    })
+});
+
+
+exports.setGuestData = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        if(req.method !== 'POST') {
+            return res.status(401).json({
+                message: 'Not allowed'
+            })
+        }
+
+        const eventId = req.body.eventId
+        
+        const data = {
+            isComming: req.body.isComming,
+            name: req.body.name,
+            count: req.body.count,
+        }
+
+        // TODO sanitize
+
+        dbApi.setGuestData(db, eventId, data).then(() => {
+            return res.status(200).send(data)
+        }).catch((err) => {
+            console.log(err)
+            return res.status(400).json({
+                message: 'not saved'
             })
         })
     })

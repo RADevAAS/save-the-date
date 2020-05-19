13ba6df7877c26b1e114677eaa9420f34d05dcfb
Rudy <arrouasserudy@hotmail.com>
2020-05-19 23:04:59 +0300

    set Form[] in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-import { getEventPublicData } from '../../api/api'
+import { getEventPublicData, setGuestData } from '../../api/api'
 
-import data from '../../data/data';
+// import data from '../../data/data';
 import TemplateA from '../Templates/TemplateA';
 import TemplateB from '../Templates/TemplateB';
 import TemplateC from '../Templates/TemplateC';
+
+import FormA from '../Forms/FormA';
+import FormB from '../Forms/FormB';
+import FormC from '../Forms/FormC';
+
 
 const _Template = {
     a: TemplateA,
@@ -13,12 +18,25 @@ const _Template = {
     c: TemplateC,
 }
 
+const _Form = {
+    a: FormA,
+    b: FormB,
+    c: FormC,
+}
+
 class App extends React.Component {
+
+    state = {
+        data: {}
+    }
 
     async componentDidMount() {
         const {match: {params: {eventId}}} = this.props
         try {
             const eventData = await getEventPublicData(eventId)
+
+            this.setState({data: eventData})
+
             console.log('eventData', eventData)    
         } catch (e) {
             console.log('No data', e)    
@@ -26,12 +44,20 @@ class App extends React.Component {
 
     }
 
+    renderForm = () => {
+        const { form } = this.state.data;
+
+        const Form = _Form[form];
+        return <Form data={this.state.data}/>
+    }
+
     render() {        
-        const { template } = data;
+        const { template, form } = this.state.data;
         const Template = _Template[template];
+        const Form = _Form[form];
 
         return (
-            <Template data={data}/>
+            <Template data={this.state.data} renderForm={this.renderForm}/>
         );
 
     }

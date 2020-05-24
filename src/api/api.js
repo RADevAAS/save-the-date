280ac2e994fa4035b5cd7a056bb8faad36841efe
Rudy <arrouasserudy@hotmail.com>
2020-05-25 01:43:00 +0300

    add endpoints

--- src/api/api.js
+++ src/api/api.js
@@ -3,8 +3,20 @@ import axios from 'axios'
 // const baseURL = process.env.REACT_APP_BASE_URL
 const baseURL = 'http://localhost:5001/save-the-date-201b5/us-central1'
 
+export const helloWorld = eventId => {
+    return axios.get(`${baseURL}/helloWorld`, {
+        params: { eventId }
+    });
+}
+
 export const getEventPublicData = eventId => {
     return axios.get(`${baseURL}/getPublicEvent`, {
+        params: { eventId }
+    });
+}
+
+export const getGuestList = eventId => {
+    return axios.get(`${baseURL}/getGuestList`, {
         params: { eventId }
     });
 }
@@ -24,4 +36,35 @@ export const setGuestData = (eventId, data) => {
             ...data
          }
     });
+}
+
+export const createEvent = (data) => {
+    return axios({
+        method: 'POST',
+        url: `${baseURL}/createEvent`,
+        data: { 
+                userId: 'abcdefg', 
+                template: 'a', 
+                form: 'a',
+                brideName: 'req.body.brideName',
+                groomName: 'req.body.groomName', 
+                ts: Date.now() - 10000, 
+                mail: 'req.body.mail',
+         }
+        }
+);
+}
+
+
+export const updateEvent = (data) => {
+    return axios({
+        method: 'PATCH',
+        url: `${baseURL}/updateEvent`,
+        data: { 
+                userId: 'abcdefg', 
+                template: 'b', 
+                brideName: 'brideNewName',
+         }
+        }
+);
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,13 @@ import {connect} from 'react-redux'
 import styles from './Login.module.css'
 import { signIn } from "../../firebase";
 import { getUserId } from "../../reducers/user";
+import {
+	getEventPublicData,
+	createEvent,
+	getGuestList,
+	setGuestData,
+	updateEvent,
+} from "../../api/api"
 
 class Login extends React.Component {
 
@@ -22,6 +29,40 @@ class Login extends React.Component {
 	};
 
 
+	getEventPublicData = async () => {
+		const x = await getEventPublicData('abcdefg')
+		console.log('getEventPublicData', x)
+	}
+
+
+	getGuestList = async () => {
+		const x = await getGuestList('abcdefg')
+		console.log('getGuestList', x)
+	}
+	
+	setGuestData = async () => {
+		const x = await setGuestData('abcdefg', {
+			isComming: true,
+            name: 'name1',
+			count: Date.now(),
+			tel: Date.now()
+		})
+		console.log('setGuestData', x)
+	}
+
+
+	createEvent = async () => {
+		const x = await createEvent()
+		console.log('createEvent', x)
+	}
+
+
+	updateEvent = async () => {
+		const x = await updateEvent()
+		console.log('updateEvent', x)
+	}
+
+
     onChangeHandler = (event) => {
     	const {name, value} = event.currentTarget;
 
@@ -35,7 +76,6 @@ class Login extends React.Component {
     render() {
       const {email, password, error} = this.state
       const {userId} = this.props
-      console.log('dddd', userId)
         if (userId) {
           return <Redirect to={`/admin`} />
 		    }
@@ -72,6 +112,13 @@ class Login extends React.Component {
                     Sign in
                   </button>
                 </form>
+              </div>
+              <div>
+				<button onClick={this.getEventPublicData}>getEventPublicData</button>
+				<button onClick={this.createEvent}>createEvent</button>
+				<button onClick={this.getGuestList}>getGuestList</button>
+				<button onClick={this.setGuestData}>setGuestData</button>
+				<button onClick={this.updateEvent}>updateEvent</button>
               </div>
             </div>
           );

a6271f813737e08445d746b3fbbd7967ebf878d1
unknown <yohavbenoliel@gmail.com>
2020-05-27 09:08:02 +0200

    css in progress

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import style from "./Home.module.css";
 
 export class Home extends Component {
     render() {
         return (
             <div>
-                NEW Home
+                <div className={style.title}>
+                    SAVE THE DATE
+                </div>
+                <div className={style.description}>
+                    description description description description 
+                    description description description description
+                    description description description description
+                </div>
+            <div className={style.images}>
+                <span className={style.imagesSegment}>images</span>
+                <span className={style.imagesSegment}>images</span>
+                <span className={style.imagesSegment}>images</span>            
+            </div>
+            <button>Contact us</button>
             </div>
         )
     }

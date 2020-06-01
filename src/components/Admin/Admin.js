f4b8e169a83d83c40bf92674e3e496ff5b63a131
unknown <yohavbenoliel@gmail.com>
2020-06-02 00:27:41 +0300

    admin page logic added + text for home page FR

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,32 +1,51 @@
 import React from "react";
-import {connect} from 'react-redux'
-import { Redirect } from 'react-router-dom'
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 import { getUserId } from "../../reducers/user";
-import {signOut} from '../../firebase'
-import UserForm from "../UserForm/UserForm"
-
+import { signOut } from "../../firebase";
+import UserForm from "../UserForm/UserForm";
+import AdminPage from "./AdminPage";
+import { getEventPublicData } from "../../api/api";
 
 class Admin extends React.Component {
+  state = {
+    eventExist: false,
+  };
 
-    logout = () => {
-        signOut()
+  logout = () => {
+    signOut();
+  };
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { eventId },
+      },
+    } = this.props;
+    try {
+      await getEventPublicData(eventId);
+
+      this.setState({ eventExist: true });
+    } catch (e) {
+      console.log("No data", e);
     }
+  }
 
-    render() {
-        const { userID } = this.props
-        if (!userID) {
-            return <Redirect to='/login' />
-        }
-
-        return (
-            <UserForm/>
-          );
+  render() {
+    const { userID } = this.props;
+    const { eventExist } = this.state;
+    if (!userID) {
+      return <Redirect to="/login" />;
+    } else if (eventExist) {
+      return <AdminPage />;
+    } else {
+      return <UserForm />;
     }
+  }
 }
 
-const mapStateToProps = state => ({
-    userID: getUserId(state)
-})
-
+const mapStateToProps = (state) => ({
+  userID: getUserId(state),
+});
 
 export default connect(mapStateToProps)(Admin);

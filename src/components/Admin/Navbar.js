6ddac7066b90249a65e8c875d7fe45ee98d71e70
unknown <yohavbenoliel@gmail.com>
2020-06-05 08:53:21 +0200

    css changes

--- src/components/Admin/Navbar.js
+++ src/components/Admin/Navbar.js
@@ -3,32 +3,76 @@ import style from "./AdminPage.module.css";
 
 export class Navbar extends Component {
 
-render() {
- 
-const { switchStep, userStep } = this.props;
+  navTitle(userStep) {
+    switch (userStep) {
+      case 1:
+        return <div>EventView</div>;
+      case 2:
+        return <div>GuestsList</div>;
+      case 3:
+        return <div>Budget</div>;
+      case 4:
+        return <div>House</div>;
+      case 5:
+        return <div>Preference</div>;
+      case 6:
+        return <div>ContactUs</div>;
+      default:
+        return <div>Navbar</div>;
+    }
+  }
+
+  render() {
+    const { switchStep, userStep } = this.props;
 
     return (
       <div>
-        <div className={style.navTitle}>Nav Bar</div>
+        <div className={style.navTitle}>{this.navTitle(userStep)}
+        <div className={style.burger}></div>
+        </div>
         <div className={style.nav}>
-          <div>
+          <div >
             <div className={style.menu}>
-              <li id="1" className={userStep === 1 ? style.selected : style.unselected} onClick={() => switchStep(1)}>
+              <li
+                id="1"
+                className={userStep === 1 ? style.selected : style.unselected}
+                onClick={() => switchStep(1)}
+              >
                 Event View
               </li>
-              <li id="2" className={userStep === 2 ? style.selected : style.unselected} onClick={() => switchStep(2)}>
+              <li
+                id="2"
+                className={userStep === 2 ? style.selected : style.unselected}
+                onClick={() => switchStep(2)}
+              >
                 Guests List
               </li>
-              <li id="3" className={userStep === 3 ? style.selected : style.unselected} onClick={() => switchStep(3)}>
+              <li
+                id="3"
+                className={userStep === 3 ? style.selected : style.unselected}
+                onClick={() => switchStep(3)}
+              >
                 Budget
               </li>
-              <li id="4" className={userStep === 4 ? style.selected : style.unselected} onClick={() => switchStep(4)}>
+              <li
+                id="4"
+                className={userStep === 4 ? style.selected : style.unselected}
+                onClick={() => switchStep(4)}
+              >
                 House
               </li>
-              <li id="5" className={userStep === 5 ? style.selected : style.unselected} onClick={() => switchStep(5)}>
+              <li
+                id="5"
+                className={userStep === 5 ? style.selected : style.unselected}
+                onClick={() => switchStep(5)}
+              >
                 Preferences
               </li>
-              <li id="6" className={userStep === 6 ? style.selected : style.unselected} onClick={() => switchStep(6)}>
+              <li
+                id="6"
+                className={userStep === 6 ? style.selected : style.unselected}
+                onClick={() => switchStep(6)}
+              >
                 Contact Us
               </li>
             </div>

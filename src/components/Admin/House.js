5cd05040c48ff3d352deda4580aa9fcb87412727
unknown <yohavbenoliel@gmail.com>
2020-06-12 09:23:05 +0200

    pie

--- src/components/Admin/House.js
+++ src/components/Admin/House.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ProgressBar from "./ProgressBar";
+import { PieChart } from "react-minimal-pie-chart";
 
 const testData = [{ completed: 60 }, { completed: 30 }, { completed: 53 }];
 
@@ -15,6 +16,16 @@ export class House extends Component {
             completed={item.completed}
           />
         ))}
+        <PieChart
+          data={[
+            { title: "One", value: 10, color: "#E38627" },
+            { title: "Two", value: 15, color: "#C13C37" },
+            { title: "Three", value: 20, color: "#6A2135" },
+          ]}
+        />
+        ;
+
+        <div>https://toomuchdesign.github.io/react-minimal-pie-chart/index.html?path=/story/interaction--click-mouseover-mouseout-callbacks</div>
       </div>
     );
   }

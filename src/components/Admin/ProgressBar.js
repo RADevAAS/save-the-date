aaba919360f4ea866fc42cc36bb271ffdf4c5205
unknown <yohavbenoliel@gmail.com>
2020-06-15 08:49:47 +0200

    animated progress bar

--- src/components/Admin/ProgressBar.js
+++ src/components/Admin/ProgressBar.js
@@ -2,17 +2,15 @@ import React from "react";
 import style from "./ProgressBar.module.css";
 
 const ProgressBar = (props) => {
-    const { completed } = props;
-  
-    
-  
-    return (
-      <div className={style.container}>
-        <div className={style.filler} style={{width: `${completed}%`}} >
-          <span className={style.label}>{`${completed}%`}</span>
-        </div>
+  const { completed } = props;
+
+  return (
+    <div className={style.container}>
+      <div className={style.filler} style={{ width: `${completed}%` }}>
+        <span className={style.label}>{`${completed}%`}</span>
       </div>
-    );
-  };
-  
-  export default ProgressBar;
+    </div>
+  );
+};
+
+export default ProgressBar;
